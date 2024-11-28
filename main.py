c5bf0f0733a1995ad048d59302a979bb66dabc29
ErnestasTamosiunas <ernestas.fighting@gmail.com>
2024-11-28 16:39:07 +0200

    Setting up the initial black screen in a game loop

--- main.py
+++ main.py
@@ -4,10 +4,22 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Setting up a screen
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Game loop
+    while True:
+        # Filling the screen with solid black
+        pygame.Surface.fill(screen, color="black") # Experimenting
+
+        # Refreshing the screen
+        pygame.display.flip()
 
 # Ensures that main() is only called when this file is run directly
 if __name__ == "__main__":
