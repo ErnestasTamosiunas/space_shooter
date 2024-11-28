a62599ee90928e149638370dbfdc656a69bfbc48
ErnestasTamosiunas <ernestas.fighting@gmail.com>
2024-11-28 16:22:27 +0200

    Adding constants and checking screen size in main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # this allows to use code from open-source
 # pygame library throughout this file
 import pygame
+from constants import *
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 # Ensures that main() is only called when this file is run directly
