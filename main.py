09171113d8bad8e0de778262fd0c7f9fc0ca7bfe
ErnestasTamosiunas <ernestas.fighting@gmail.com>
2024-11-28 16:16:49 +0200

    Adding main.py for a runnable program

---
+++ main.py
@@ -0,0 +1,11 @@
+# this allows to use code from open-source
+# pygame library throughout this file
+import pygame
+
+def main():
+    print("Starting asteroids!")
+
+
+# Ensures that main() is only called when this file is run directly
+if __name__ == "__main__":
+    main()
